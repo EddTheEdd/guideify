d785a32ffdd94a9cf1b16e700203a294aaaf4415
EddTheEdd <eddbaumanis@gmail.com>
2023-11-29 09:53:40 +0200

    attempt at fixing migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,6 +1,4 @@
-// const dotenv = require("dotenv");
 
-// dotenv.config();
 console.log(process.env.DB_USER);
 // knexfile.js
 module.exports = {
@@ -13,7 +11,25 @@ module.exports = {
       password: process.env.DB_PASSWORD,
     },
     migrations: {
-      directory: "./db/migrations", // Specify the directory where your migrations are stored
+      directory: "./db/migrations",
     },
   },
+  production: {
+    client: "pg",
+    connection: {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT || 5432,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  }
 };
