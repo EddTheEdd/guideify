574a6da813c99510daa4d21fa3259408645a6e85
EddTheEdd <eddbaumanis@gmail.com>
2023-12-09 22:03:51 +0200

    more db config

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,14 +1,17 @@
 
 console.log(process.env.DB_USER);
 // knexfile.js
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
 module.exports = {
   development: {
     client: "pg",
     connection: {
-      user: process.env.DB_USER,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
+      host: PGHOST,
+      user: PGUSER,
+      password: PGPASSWORD,
+      database: PGDATABASE,
     },
     migrations: {
       directory: "./db/migrations",
@@ -17,10 +20,10 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      user: process.env.DB_USER,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
+      host: PGHOST,
+      user: PGUSER,
+      password: PGPASSWORD,
+      database: PGDATABASE,
       port: process.env.DB_PORT || 5432,
       ssl: { rejectUnauthorized: false }
     },
