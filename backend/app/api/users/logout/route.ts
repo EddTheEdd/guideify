f03df0817fe1e35b84a8c6836aa41fad456baee8
EddTheEdd <eddbaumanis@gmail.com>
2023-12-11 18:11:05 +0200

    no cache logout, needs to be implemented everywhere almost

--- backend/app/api/users/logout/route.ts
+++ backend/app/api/users/logout/route.ts
@@ -3,12 +3,16 @@ import { NextResponse } from "next/server";
 export async function GET() {
     try {
         const response = NextResponse.json({
-            message: "Lougout successful",
+            message: "Logout successful",
             success: true
-        })
-        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0)});
+        });
+
+        // Set the Cache-Control header to no-cache
+        response.headers.set('Cache-Control', 'no-cache');
+
+        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
         return response;
     } catch (error: any) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
